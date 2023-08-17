ecb437f714ad2c4e706a26a7b9728c9172fde4ae
ronaldo araujo <ronaldoaraujo@jet.com.br>
2023-08-17 20:14:55 -0300

    integrar vm e ligando com service

--- lib/src/features/auth/login/login_page.dart
+++ lib/src/features/auth/login/login_page.dart
@@ -1,16 +1,21 @@
 import 'package:dw_barbershop/src/core/ui/constants.dart';
 import 'package:dw_barbershop/src/core/ui/helpers/form_helper.dart';
+import 'package:dw_barbershop/src/features/auth/login/login_state.dart';
+import 'package:dw_barbershop/src/features/auth/login/login_vm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:validatorless/validatorless.dart';
 
-class LoginPage extends StatefulWidget {
+import '../../../core/ui/helpers/messages.dart';
+
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  ConsumerState<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends ConsumerState<LoginPage> {
   final formKey = GlobalKey<FormState>();
   final emailEC = TextEditingController();
   final passwordEC = TextEditingController();
@@ -24,6 +29,26 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final LoginVm(:login) = ref.watch(loginVmProvider.notifier);
+
+    ref.listen(loginVmProvider, (_, state) { 
+         switch (state) {
+      case LoginState(status: LoginStateStatus.initial): 
+        break;      
+        case LoginState(status: LoginStateStatus.error, :final errorMessage?): 
+        Messages.showError(errorMessage, context);
+
+        case LoginState(status: LoginStateStatus.error): 
+        Messages.showError('Erro ao realizar login', context);
+        case LoginState(status: LoginStateStatus.admLogin): 
+
+        case LoginState(status: LoginStateStatus.employerLogin): 
+
+      default:
+    }
+    });
+ 
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Form(
@@ -108,7 +133,16 @@ class _LoginPageState extends State<LoginPage> {
                           ElevatedButton(
                               style: ElevatedButton.styleFrom(
                                   minimumSize: const Size.fromHeight(56)),
-                              onPressed: () {},
+                              onPressed: () {
+                                switch (formKey.currentState?.validate()) {
+                                  case (false || null):
+                                    //mostrar uma menssagem de erro
+                                    Messages.showError('Campos invalidos', context);
+                                    break;
+                                  case true:
+                                  login(emailEC.text, passwordEC.text);
+                                }
+                              },
                               child: const Text(
                                 'ACESSAR',
                               ))
