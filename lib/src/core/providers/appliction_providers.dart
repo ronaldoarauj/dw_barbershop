0217c0034a3737dc5745eba81a97533f75dbe8b3
ronaldo araujo <ronaldoaraujo@jet.com.br>
2023-08-18 20:28:20 -0300

    provider do usuario

--- lib/src/core/providers/appliction_providers.dart
+++ lib/src/core/providers/appliction_providers.dart
@@ -1,4 +1,6 @@
+import 'package:dw_barbershop/src/core/fp/either.dart';
 import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
+import 'package:dw_barbershop/src/model/user_model.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository.dart';
 import 'package:dw_barbershop/src/repositories/user/user_repository_impl.dart';
 import 'package:dw_barbershop/src/services/user_login/user_login_service.dart';
@@ -12,8 +14,19 @@ part 'appliction_providers.g.dart';
 RestClient restClient(RestClientRef ref) => RestClient();
 
 @Riverpod(keepAlive: true)
-UserRepository userRepository(UserRepositoryRef ref) => UserRepositoryImpl(restClient: ref.read(restClientProvider));
+UserRepository userRepository(UserRepositoryRef ref) =>
+    UserRepositoryImpl(restClient: ref.read(restClientProvider));
 
 @Riverpod(keepAlive: true)
-UserLoginService userLoginService(UserLoginServiceRef ref) => UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
+UserLoginService userLoginService(UserLoginServiceRef ref) =>
+    UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
 
+@Riverpod(keepAlive: true)
+Future<UserModel> getMe(GetMeRef ref) async {
+  final result = await ref.watch(userRepositoryProvider).me();
+
+  return switch (result) {
+    Success(value: final userModel) => userModel,
+    Failure(:final exception) => throw exception,
+  };
+}
